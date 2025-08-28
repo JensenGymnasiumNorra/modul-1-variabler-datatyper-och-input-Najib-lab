1ad1bac14b43252441a47679b07c19033ca6bb52
Najib-lab <najabd080302@student.jenseneducation.se>
2025-08-28 09:49:31 +0200

    födelseår

--- uppg1_utskrift.py
+++ uppg1_utskrift.py
@@ -5,5 +5,8 @@ Programmet ska sedan skriva ut en mening som använder dessa variabler
 Exempel på utskrift, det inom () ska ändras om variablerna ändras:
 (Hampus) gillar att (kolla på film) och (spela kortspel)
 """
-
+name="NAjib"
+sak="spela fotboll"
+sak2="Titta på serier"
+print(f"{name} gillar att {sak} och {sak2}")
 
